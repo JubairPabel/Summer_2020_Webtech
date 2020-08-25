daba76057e3a2af4f0b26fe4d1907e9bfab21284
Jubair Uddin Pabel <gtxbiscuit@gmail.com>
2020-08-25 09:07:59 +0600

    Email Validation

---
+++ Final_Lab_Task_3/script.js
@@ -0,0 +1,74 @@
+
+
+function validate ()
+{
+	var username = document.getElementById('name').value;
+   
+    if(username == ""){
+		document.getElementById('nameMsg').innerHTML = "username can't left empty";
+		return false;
+
+	}
+
+    
+    
+
+
+    var str;
+		var t=1;
+		str =document.getElementById('Email').value;
+         var res = str.split(‘@’);
+         
+		var part1=res[0];
+		var part2=res[1];
+        var dotsplt=part2.split(‘.’);
+
+		if(document.getElementById('Email').value==”")
+		{
+		alert(“Empty”);
+
+		}
+        else if(str.split(‘@’).length!=2)
+		{
+			document.getElementById('emailMsg').innerHTML = "zero @ OR morethan one @";
+		    return false;
+		}
+
+
+		
+		else if(part1.length==0)
+		{
+				document.getElementById('emailMsg').innerHTML = "no content before @";
+		    return false;
+		}
+		else if(part1.split(” “).length>2)
+		{
+			document.getElementById('emailMsg').innerHTML = "Space Befor @!";
+		  return false;
+		}
+
+		
+		  
+		else if(part2.split(“.”).length<2)
+		{
+			document.getElementById('emailMsg').innerHTML = "dot missing";
+			return false;
+		
+		}
+		else if(dotsplt[0].length==0 )
+		{
+			document.getElementById('emailMsg').innerHTML = "no content b/w @ and dot";
+			return false;
+	
+		}
+		else if(dotsplt[1].length<2 ||dotsplt[1].length>4)
+		{
+			document.getElementById('emailMsg').innerHTML = "err aftr dot";
+			return false;
+
+		}
+		else {
+			return false;
+		}
+
+}
