a4c099d27090e2c131fd6cb32f68cc302d7ab7a8
Jubair Uddin Pabel <gtxbiscuit@gmail.com>
2020-08-31 20:30:21 +0600

    email done

---
+++ Final_Lab_Task_2/emailjs.js
@@ -0,0 +1,57 @@
+
+
+
+
+ function validate()
+ {
+ 	str =document.getElementById('Email').value;
+
+ var res = str.split(‘@’);
+
+if(document.getElementById('Email').value==”")
+		{
+		alert(“Empty”);
+
+		}
+        else if(str.split(‘@’).length!=2)
+		{
+			document.getElementById('emailMsg').innerHTML = "zero @ OR morethan one @";
+		    return false;
+		}
+
+
+		
+		else if(part1.length==0)
+		{
+				document.getElementById('emailMsg').innerHTML = "no content before @";
+		    return false;
+		}
+		else if(part1.split(” “).length>2)
+		{
+			document.getElementById('emailMsg').innerHTML = "Space Befor @!";
+		  return false;
+		}
+
+		
+		  
+		else if(part2.split(“.”).length<2)
+		{
+			document.getElementById('emailMsg').innerHTML = "dot missing";
+			return false;
+		
+		}
+		else if(dotsplt[0].length==0 )
+		{
+			document.getElementById('emailMsg').innerHTML = "no content b/w @ and dot";
+			return false;
+	
+		}
+		else if(dotsplt[1].length<2 ||dotsplt[1].length>4)
+		{
+			document.getElementById('emailMsg').innerHTML = "err aftr dot";
+			return false;
+
+		}
+
+ 
+ }
