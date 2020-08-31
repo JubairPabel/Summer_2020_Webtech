3d599eb352e0765861b00b21fcaa25a65d9d81df
Jubair Uddin Pabel <gtxbiscuit@gmail.com>
2020-08-31 20:55:56 +0600

    All done

---
+++ Final_Lab_Task_2/propjs.js
@@ -0,0 +1,31 @@
+
+
+function Validate()
+{
+    var id = document.getElementById("id").value;
+    var picture = document.getElementById("file").value;
+
+    
+    if(id=="")
+    {
+       document.getElementById("profile_Msg").innerHTML="user id field can't be empty";
+       
+    }
+
+   
+    else if (id<0) 
+    {
+        document.getElementById("profile_Msg").innerHTML="invalid !";
+    }
+
+    else if(picture=="")
+    {
+        document.getElementById("picture_Msg").innerHTML="picture can't be empty , please select any picture.";
+        
+    }
+    else
+    {
+        
+        return true;
+    }
+}
